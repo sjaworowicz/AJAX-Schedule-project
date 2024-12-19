d61cdf2a2c0c3209fb87a610823ac74f3a004973
sjaworowicz <125sjaworowicz@frhsd.com>
2024-12-19 10:55:31 -0500

    Schedule in json now coded, still working on how to make the schedule show classes

--- js/script.js
+++ js/script.js
@@ -1,57 +1,90 @@
-const array = []; 
+// https://jsonkeeper.com/b/C2DH
 
-const filteredArray = array.filter(element => {
-    return condition;
+// const array = []; 
+
+// const filteredArray = array.filter(element => {
+//   return condition;
+//  });
+
+var inputField = $("#dayInput");
+var submittedDayLetter = inputField.val();
+  var scheduleBody = $("#scheduleList");
+  var btn = $("#submitDay");
+
+  btn.on("click", function() {
+    $.ajax({
+      url: 'https://www.jsonkeeper.com/b/C2DH',
+      method: 'GET',
+      success: function(result) {
+
+      }
+    })
   });
 
-  const scheduleData = [
-    {
-      "name": "Physical Education",
-      "room": "GYM1",
-      "teacher": "Mrs. Pusz",
-      "days": ["A", "B", "C", "E", "F"]
-    },
-    {
-      "name": "Tech Systems & Design",
-      "room": "Room C106",
-      "teacher": "Mr. Ertel",
-      "days": ["A", "B", "D", "E", "F"]
-    },
-    {
-        "name": "Quantitative Reasoning",
-        "room": "Room B207",
-        "teacher": "Mrs. Heaney",
-        "days": ["A", "C", "D", "E", "G"]
-      },
-      {
-        "name": "Honors NJIT IS219 Advanced Website Development W",
-        "room": "Room C104",
-        "teacher": "Mrs. McDonalds",
-        "days": ["B", "C", "D", "F", "G"]
-      },
-      {
-        "name": "Graphic Design & Photography",
-        "room": "Room C104",
-        "teacher": "Mrs. McDonald",
-        "days": ["A", "B", "D", "E", "G"]
-      },
-      {
-        "name": "BCC ENG121/155",
-        "room": "Room B108",
-        "teacher": "Mrs. Robinson",
-        "days": ["A", "B", "D", "F", "G"]
-      },
-      {
-        "name": "Band 2",
-        "room": "Room A101",
-        "teacher": "Mr. Gross",
-        "days": ["B", "C", "E", "F", "G"]
-      },
-  ];
-  
-  function getClassesForDay(day) {
-    return scheduleData.filter(classInfo => classInfo.days.includes(day));
-  }
+  inputField.change(function(){
+    var submittedDayLetter = inputField.val();
+    console.log(submittedDayLetter);
+  });
 
-  const aDayClasses = getClassesForDay('A');
-  console.log(aDayClasses);
+ // const scheduleData = [
+ //   {
+ //     "name": "Physical Education",
+ //     "room": "GYM1",
+ //     "teacher": "Mrs. Pusz",
+ //     "days": ["A", "B", "C", "E", "F"]
+ //   },
+ //   {
+ //     "name": "Tech Systems & Design",
+ //     "room": "Room C106",
+ //     "teacher": "Mr. Ertel",
+ //     "days": ["A", "B", "D", "E", "F"]
+ //   },
+ //   {
+ //       "name": "Quantitative Reasoning",
+ //       "room": "Room B207",
+ //       "teacher": "Mrs. Heaney",
+ //       "days": ["A", "C", "D", "E", "G"]
+ //     },
+ //     {
+ //       "name": "Honors NJIT IS219 Advanced Website Development W",
+ //       "room": "Room C104",
+ //       "teacher": "Mrs. McDonalds",
+ //       "days": ["B", "C", "D", "F", "G"]
+ //     },
+ //     {
+ //       "name": "Graphic Design & Photography",
+ //       "room": "Room C104",
+ //       "teacher": "Mrs. McDonald",
+ //       "days": ["A", "B", "D", "E", "G"]
+ //     },
+ //     {
+ //       "name": "BCC ENG121/155",
+ //       "room": "Room B108",
+ //       "teacher": "Mrs. Robinson",
+ //       "days": ["A", "B", "D", "F", "G"]
+ //     },
+ //     {
+ //       "name": "Band 2",
+ //       "room": "Room A101",
+ //       "teacher": "Mr. Gross",
+ //       "days": ["B", "C", "E", "F", "G"]
+  //    },
+ // ];
+
+  
+  function appendClasses(data) {
+    var trl = $("<th></th>").text(data[i].className);
+    var trl = $("<th></th>").text("");
+    var trl = $("<th></th>").text("");
+    var trl = $("<th></th>").text("");
+    var trl = $("<th></th>").text("");
+
+
+
+
+    console.log(result.className);
+    scheduleBody.append()
+  };
+
+  // const aDayClasses = getClassesForDay('A');
+  // console.log(aDayClasses);
